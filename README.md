798b26161bc964cefdefb608a014e308cc99e77c
Nitza <rondon.nitza@gmail.com>
2019-04-01 20:23:31 -0400

    Updated read.me and updated liri

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # liri-node-app
+
+### Overview of Project
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+### What this project uses
+This project uses Nnodejs, Moment.js, JavaScript, functions, NPM: axios, node-spotify-api, dotenv
+
+### How it functions
+From terminal you will use a command and user input to retrieve data from APIs. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+Type in the command line 'node liri.js' and one of the following commands and the input text:
+* Movies command: movie-this
+Will retrieve the following:
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+* Songs command: spotify-this-song
+Will retrieve the following:
+    ```
+     * Artist(s)
+     * The song's name
+     * A preview link of the song from Spotify
+     * The album that the song is from
+    ```
+* Concerts command: concert-this
+Will retrieve the following:
+    ```
+    *Name of the venue
+    *Venue location
+    *Date of the Event (using moment to format)
+    ```
+* Do What It Says (FS file) command: do-what-it says 
+Will retrieve the the text inside of a file named random.txt and then use it to call one of LIRI's commands.
--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-// environment variables
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
@@ -7,71 +6,77 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
-
 var command = process.argv[2];
 var userInputText = process.argv.slice(3).join(" ");
 
+function checkCommand(action, input) {
+    switch (action) {
+        case "concert-this":
+            concerts(input);
+            break;
 
-switch (command) {
-    case "concert-this":
-        concerts();
-        break;
+        case "spotify-this-song":
+            spotifySong(input);
+            break;
 
-    case "spotify-this-song":
-        spotifySong();
-        break;
+        case "movie-this":
+            movieThis(input);
+            break;
 
-    case "movie-this":
-        movie();
-        break;
+        case "do-what-it-says":
+            doIt();
+            break;
 
-    case "do-what-it-says":
-        doIt();
-        break;
-
-    default:
-        console.log("Need to know how Liri.js works? Type in the command line 'node liri.js' and one of the following commands and the input text:");
-        console.log("\r 'concert-this' and the name of a band");
-        console.log("\r 'spotify-this-song' and the name of a song");
-        console.log("\r 'movie-this' and the name of a movie");
-        console.log("\r That's it! Enjoy!");
-};
-
-
-function concerts() {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + userInputText + "/events?app_id=codingbootcamp";
-    if (userInputText === "") {
-        console.log("You must enter a band/artist");
-    } else {
-        axios.get(queryUrl).then(
-            function (response) {
-                for (i = 0; i < response.data.length; i++) {
-                    console.log("\r\n------YOUR CONCERT SEARCH for " + userInputText + "---------");
-                    console.log("Venue: " + response.data[i].venue.name);
-                    var location = response.data[i].venue.city + ", " + response.data[i].venue.region + " " + response.data[i].venue.country
-                    console.log("Location: " + location);
-                    var eventDate = moment(response.data[i].venue.datetime).format("MM-DD-YYYY");
-                    console.log("Date: " + eventDate);
-                }
-            }
-        );
+        default:
+            console.log("Need to know how Liri.js works? Type in the command line 'node liri.js' and one of the following commands and the input text:");
+            console.log("\r 'concert-this' and the name of a band");
+            console.log("\r 'spotify-this-song' and the name of a song");
+            console.log("\r 'movie-this' and the name of a movie");
+            console.log("\r That's it! Enjoy!");
     };
 };
 
+checkCommand(command, userInputText);
 
+function concerts(band) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp";
+    if (band === "") {
+        console.log("You must enter a band/artist");
+    } else {
+        axios.get(queryUrl).then(
+                function (response) {
+                    if (response.data.length === 0) {
+                        console.log("Sorry, no information. Try another.");
+                    } else {
+                        for (var i = 0; i < response.data.length; i++) {
+                            console.log("\r\n------CONCERT SEARCH for " + band + "---------");
+                            console.log("Venue: " + response.data[i].venue.name);
+                            var location = response.data[i].venue.city + ", " + response.data[i].venue.region + " " + response.data[i].venue.country
+                            console.log("Location: " + location);
+                            var eventDate = moment(response.data[i].datetime).format("MM-DD-YYYY");
+                            console.log("Date: " + eventDate);
+                        }
+                    }
+                }
+            )
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+};
 
-function spotifySong() {
-    if (userInputText === "") {
-        userInputText = "The Sign by Ace of Base";
+function spotifySong(song) {
+    if (song === "") {
+        song = "The Sign: Ace of Base";
     }
     spotify
         .search({
-            type: "track",
-            query: userInputText
+            type: "artist,track",
+            query: song
         })
         .then(function (response) {
-            for (i = 0; i <= 7; i++) {
-                console.log("\r\n------YOUR SONG SEARCH for '" + userInputText + "'---------");
+            for (var i = 0; i < 7; i++) {
+                console.log("\r\n------SONG SEARCH for '" + song + "'---------");
                 console.log("Artist: " + response.tracks.items[i].album.artists[0].name);
                 console.log("Album: " + response.tracks.items[i].album.name);
                 console.log("Song: " + response.tracks.items[i].name);
@@ -83,18 +88,16 @@ function spotifySong() {
         });
 };
 
-
-
-function movie() {
-    var queryUrl = "http://www.omdbapi.com/?t=" + userInputText + "&y=&plot=short&apikey=trilogy";
-    if (userInputText === "") {
-        userInputText = "Mr. Nobody";
+function movieThis(movie) {
+    if (movie === "") {
+        movie = "Mr. Nobody";
         console.log("If you haven't watched 'Mr. Nobody', then you should: <http://www.imdb.com/title/tt0485947/>");
         console.log("It's on Netflix!");
-    } else {
-        axios.get(queryUrl).then(
+    };
+
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy").then(
             function (response) {
-                console.log("\r\n------YOUR MOVIE SEARCH for " + userInputText + "---------");
+                console.log("\r\n------MOVIE SEARCH for " + movie + "---------");
                 console.log("Movie Title: " + response.data.Title);
                 console.log("Release Year: " + response.data.Year);
                 console.log("IMDB Rating: " + response.data.Ratings[0].Value);
@@ -104,27 +107,18 @@ function movie() {
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
             }
-        );
-    };
+        )
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
-// ***********************************************************************************
-
-// DO WHAT IT SAYS
-// if request is do-what-it-says get artist from bands in town - fs
-// `node liri.js do-what-it-says`
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-// ***********************************************************************************
-
 function doIt() {
-    // We will read the existing  file
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
             return console.log(err);
         }
-        userInputText = data;
-        spotifySong ();
+        var file = data.split(",");
+        checkCommand(file[0], file[1]);
     });
-}
+};
